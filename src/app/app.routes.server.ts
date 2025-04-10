9b08281a307778b6bf55cbe1d7ace26a5e0ea357
Hasanur Rahman Shishir <1Shishir@codixels-Mac-mini.local>
2025-04-11 02:16:44 +0600

    before adding env

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,28 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // {
+  //   path: 'prescription/view/:prescriptionId',
+  //   renderMode: RenderMode.Server
+  // },
+  // {
+  //   path: 'prescription/new/:patientId',
+  //   renderMode: RenderMode.Server
+  // },
+  // {
+  //   path: 'prescription/edit/:prescriptionId',
+  //   renderMode: RenderMode.Server
+  // },
+  // {
+  //   path: 'prescription/print/:prescriptionId',
+  //   renderMode: RenderMode.Server
+  // },
+   {
+    path: 'prescription/**',
+    renderMode: RenderMode.Server
+  }, 
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
