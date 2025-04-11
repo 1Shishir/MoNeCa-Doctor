3b6c89cb73ac012f1a21569fcfe733abc19242cd
Hasanur Rahman Shishir <1Shishir@codixels-Mac-mini.local>
2025-04-11 23:13:31 +0600

    env manually added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env['FIREBASE_API_KEY'] || '',
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
-    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
-    appId: process.env['FIREBASE_APP_ID'] || '',
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || ''
+    apiKey: "AIzaSyCZduDRlL16BjzBEaxCqvnJlE0oIk-OlQY",
+    authDomain: "project-moneca.firebaseapp.com",
+    projectId: "project-moneca",
+    storageBucket: "project-moneca.firebasestorage.app",
+    messagingSenderId: "431559302603",
+    appId: "1:431559302603:web:b196ddc8c38d6c05a0aa4e",
+    measurementId: "G-792YVZ1XRS"
   }
+  // firebase: {
+  //   apiKey: process.env['FIREBASE_API_KEY'] || '',
+  //   authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
+  //   projectId: process.env['FIREBASE_PROJECT_ID'] || '',
+  //   storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
+  //   messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
+  //   appId: process.env['FIREBASE_APP_ID'] || '',
+  //   measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || ''
+  // }
 };
